25b1d66ff8acd58416c96cfbc4339280e5383a2c
Umesh02 <u.teach.and.i.learn@gmail.com>
2018-10-19 16:54:52 +0530

    End of S1-L4

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const getCurrentDate = () => {
-  const date = new Date();
-  return date.toDateString();
-};
-const greeting = (
-  <h1>Hello, Fellow Udemy Learners! Current date is {getCurrentDate()}</h1>
-);
-ReactDOM.render(greeting, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
