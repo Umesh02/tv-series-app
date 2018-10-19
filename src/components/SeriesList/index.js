1d9b0d7a5886c79b4ebba306304a8efe02e93317
Umesh02 <u.teach.and.i.learn@gmail.com>
2018-10-19 20:12:32 +0530

    End of S2-L12

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import "./index.css";
 
-const SeriesListItem = ({ series }) => (
-  <li key={series.show.id}>{series.show.name}</li>
-);
+const SeriesListItem = ({ series }) => <li>{series.show.name}</li>;
 
 const SeriesList = props => {
   return (
     <div>
       <ul className="series-list">
         {props.list.map(series => (
-          <SeriesListItem series={series} />
+          <SeriesListItem series={series} key={series.show.id} />
         ))}
       </ul>
     </div>
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -5,16 +5,19 @@ class Series extends Component {
     series: []
   };
 
-  componentDidMount() {
-    fetch(`http://api.tvmaze.com/search/shows?q=Vikings`)
+  onSeriesInputChange = e => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
       .then(response => response.json())
       .then(json => this.setState({ series: json }));
-  }
+  };
   render() {
     return (
       <div>
         The length of the series array ={" "}
         <span className="series-length">{this.state.series.length}</span>
+        <div>
+          <input type="text" onChange={this.onSeriesInputChange} />
+        </div>
         <SeriesList list={this.state.series} />
       </div>
     );
