78b9b25231517476c3f341f3702837b5f1396562
Umesh02 <u.teach.and.i.learn@gmail.com>
2018-10-20 18:14:58 +0530

    End of S2-L17

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 
-const SeriesListItem = ({ series }) => <li>{series.show.name}</li>;
+const SeriesListItem = ({ series }) => (
+  <li>
+    <Link to={`/series/${series.show.id}`}>{series.show.name}</Link>
+  </li>
+);
 
 const SeriesList = props => {
   return (
--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,10 +1,77 @@
 import React, { Component } from "react";
+import Loader from "../../components/Loader";
+import { Link } from "react-router-dom";
 
 class SingleSeries extends Component {
+  state = {
+    show: null
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+      .then(response => response.json())
+      .then(json => this.setState({ show: json }));
+  }
+
   render() {
+    const { show } = this.state;
+    console.log(show);
+
     return (
       <div>
-        <p>Single Series - the show id will be {this.props.match.params.id}</p>
+        {show === null && <Loader />}
+        {show !== null && (
+          <div>
+            <p>
+              <span style={{ color: "#0454f5", fontWeight: "bold" }}>
+                Name -
+              </span>
+              {show.name}
+            </p>
+            <p>
+              <span style={{ color: "#0454f5", fontWeight: "bold" }}>
+                Type -
+              </span>
+              {show.type}, &nbsp;&nbsp;&nbsp;&nbsp;{" "}
+              <span style={{ color: "#0454f5", fontWeight: "bold" }}>
+                Genres -
+              </span>
+              {show.genres}
+            </p>
+            <p>
+              <span style={{ color: "#0454f5", fontWeight: "bold" }}>
+                Premiered -
+              </span>
+              {show.premiered}, &nbsp;&nbsp;&nbsp;&nbsp;
+              <span style={{ color: "#0454f5", fontWeight: "bold" }}>
+                Rating -
+              </span>
+              {show.rating.average}
+            </p>
+            <p>
+              <span style={{ color: "#0454f5", fontWeight: "bold" }}>
+                No. of Episodes -
+              </span>
+              {show._embedded.episodes.length}
+            </p>
+            <p>
+              <span style={{ color: "#0454f5", fontWeight: "bold" }}>
+                Official Site -
+              </span>
+              {show.officialSite}
+            </p>
+
+            <p>
+              <img alt="Show" src={show.image.medium} />
+            </p>
+            <button>
+              <Link to={`/`} style={{ color: "white" }}>
+                Back to Search
+              </Link>
+            </button>
+          </div>
+        )}
       </div>
     );
   }
